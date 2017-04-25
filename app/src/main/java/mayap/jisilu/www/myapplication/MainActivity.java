4351c7bcd0f2366aca3186c00f985dbcaf5a67ac
Administrator <https://github.com/Hanwenrui/jisilu2>
2017-04-25 19:56:22 +0800

    first commit

---
+++ app/src/main/java/mayap/jisilu/www/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package mayap.jisilu.www.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//kaiwanxiao
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
